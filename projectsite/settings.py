b897f2976795eda083d1a5136b3720cf0b85e033
ArbitErDL <202380330@psu.palawan.edu.ph>
2025-09-11 08:55:05 +0800

    Allowed hosts edit

--- projectsite/settings.py
+++ projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vf4wuv_v!-ztoewiuc&&ap9xz2s)h)cmnd)a%iyl*#!0yd0xyd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['Arbiterdl.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
